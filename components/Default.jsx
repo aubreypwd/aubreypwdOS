9b577c95a5cdc71ba6b68e562300e542a988cf0d
Aubrey Portwood <code@aubreypwd.com>
2021-07-24 21:27:28 -0600

    These are important right now.

--- components/Default.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Default( { slug } ) {
-
-
-	return <>
-
-		</section>
-	</>;
-};
--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,6 @@ import {
 	faTwitter,
 	faLinkedin,
 	faWordpress,
-	faStackOverflow,
 
 //https://fontawesome.com/v5.15/icons/stack-overflow?style=brands
 } from '@fortawesome/free-brands-svg-icons';
@@ -24,8 +23,9 @@ const size = 96;
 
 export default function Home() {
 	return <>
+
 		<Head>
-			<title>{config.title}</title>
+			<title>{config.author} &mdash; {config.description}</title>
 			<meta name="description" content={config.description} />
 			<link rel="icon" href={`${config.gravatar}?s=32`} />
 		</Head>
@@ -33,12 +33,11 @@ export default function Home() {
 		<FadeIn transitionDuration={400*4} delay={50*2}>
 			<div className="icons">
 				<ul>
-					<li><IconLink href="mailto:contact@aubreypwd.com" icon={faInbox} newWindow={false} title="Email Me" size={size} /></li>
-					<li><IconLink href="https://github.com/aubreypwd" icon={faGithub} newWindow={true} title="Github" size={size} /></li>
 					<li><IconLink href="https://twitter.com/aubreypwd" icon={faTwitter} newWindow={true} title="Twitter" size={size} /></li>
+					<li><IconLink href="https://github.com/aubreypwd" icon={faGithub} newWindow={true} title="Github" size={size} /></li>
 					<li><IconLink href="https://linkedin.com/in/aubreypwd" icon={faLinkedin} newWindow={true} title="LinkedIn" size={size} /></li>
 					<li><IconLink href="https://profiles.wordpress.org/aubreypwd/" icon={faWordpress} newWindow={true} title="WordPress.org" size={size} /></li>
-					<li><IconLink href="https://stackoverflow.com/users/1436129/aubreypwd" icon={faStackOverflow} newWindow={true} title="Stack Overflow" size={size} /></li>
+					<li><IconLink href="mailto:contact@aubreypwd.com" icon={faInbox} newWindow={false} title="Email" size={size} /></li>
 				</ul>
 			</div>
 		</FadeIn>
