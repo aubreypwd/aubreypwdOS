9d00ab1e41b835c5d0a5d8597cc49b2041a0ded4
Aubrey Portwood <code@aubreypwd.com>
2021-07-25 12:33:40 -0600

    I like this name a lot better.

--- components/IconLink.jsx
+++ components/IconLink.jsx
@@ -1,8 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function IconLink( { href, icon, newWindow = false, title = '', size = 96 } ) {
+export default function IconLink( {
+	href,
+	icon,
+	newWindow = false,
+	title = '',
+	size = 96,
+	description = '',
+} ) {
+
+	console.log( `${href}: ${description}` );
 
 	return <>
+
 		<a href={href} rel="noreferrer" target={ newWindow ? '_blank' : '' }>
 			<div className="icon"><FontAwesomeIcon icon={icon} height={size} width={size} /></div>
 			<div className="title">{title}</div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -14,36 +14,40 @@ import {
 import {
 	faHome,
 	faInbox,
+	faComments,
 
 // https://fontawesome.com/v5.15/icons?d=gallery&p=1&q=skull&s=solid&m=free
 } from '@fortawesome/free-solid-svg-icons';
 
 const config = {
 
-	title: "Aubrey Portwood",
+	url: "https://aubreypwd.com",
+	title: "aubreypwdOS",
 	author: "Aubrey Portwood",
-	description: "I mostly build websites",
 	gravatar: "https://www.gravatar.com/avatar/09601923fd59a7433892711376c37e41/",
 	initialIconSize: 96 * 4
 }
 
-export default function Home() {
+console.log( `%cThanks for visiting ${config.title} a simple site that links to further things about me, ${config.author}`, 'color: #ff00ff' );
+console.log( '%cBuilt with Next.js - https://github.com/aubreypwd/aubreypwdOS', 'color: #06e6f2' );
+
+export default function Index() {
 	return <>
 
 		<Head>
-			<title>{config.author} &mdash; {config.description}</title>
-			<meta name="description" content={config.description} />
+			<title>{config.title} | {config.author}</title>
+			<meta name="description" content={config.title} />
 			<link rel="icon" href={`${config.gravatar}?s=32`} />
 		</Head>
 
 		<FadeIn transitionDuration={400*4} delay={500}>
 			<div className="icons">
 				<ul>
-					<li><IconLink href="https://twitter.com/aubreypwd" icon={faTwitter} newWindow={true} title="Twitter" size={config.initialIconSize} /></li>
-					<li><IconLink href="https://github.com/aubreypwd" icon={faGithub} newWindow={true} title="Github" size={config.initialIconSize} /></li>
-					<li><IconLink href="https://linkedin.com/in/aubreypwd" icon={faLinkedin} newWindow={true} title="LinkedIn" size={config.initialIconSize} /></li>
-					<li><IconLink href="https://profiles.wordpress.org/aubreypwd/" icon={faWordpress} newWindow={true} title="WordPress.org" size={config.initialIconSize} /></li>
-					<li><IconLink href="mailto:contact@aubreypwd.com" icon={faInbox} newWindow={false} title="Email" size={config.initialIconSize} /></li>
+					<li><IconLink href="https://twitter.com/aubreypwd" icon={faTwitter} newWindow={true} title="Twitter" size={config.initialIconSize} description="I mostly do things on Twitter, you're going to get to know me more personally there."/></li>
+					<li><IconLink href="https://github.com/aubreypwd" icon={faGithub} newWindow={true} title="Github" size={config.initialIconSize} description="I code and type things in my terminal, it's all mostly on Github."/></li>
+					<li><IconLink href="https://linkedin.com/in/aubreypwd" icon={faLinkedin} newWindow={true} title="LinkedIn" size={config.initialIconSize} description="You're going to learn more about my past and my skills here, I rarely post here." /></li>
+					<li><IconLink href="https://profiles.wordpress.org/aubreypwd/" icon={faWordpress} newWindow={true} title="WordPress.org" size={config.initialIconSize} description="I've been build WordPress websites for over 10 years and try my best to stay involved in the WordPress community."/></li>
+					<li><IconLink href="https://tlk.io/aubreypwd" icon={faComments} newWindow={true} title="Chat" size={config.initialIconSize} description="If I'm at my computer, feel free to chat with me directly."/></li>
 				</ul>
 			</div>
 		</FadeIn>
